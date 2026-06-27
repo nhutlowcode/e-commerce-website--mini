9ca8dcb47362f8c8b31a747e6c75699a08f8edca
Minh Nhut <minhnhutst365@gmail.com>
2026-06-27 19:53:15 +0700

    fix env and server.js

--- shop-backend/server.js
+++ shop-backend/server.js
@@ -14,7 +14,7 @@ const app = express()
 const PORT = process.env.PORT || 5000 
 
 app.use(cors({
-    origin: process.env.CLIENT_URL, 
+    origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL_PROD], 
     credentials: true                
 }))
 
@@ -26,7 +26,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_URL,
+        origin: [process.env.CLIENT_URL_DEV, process.env.CLIENT_URL_PROD],
         methods: ["GET", "POST", "PUT", "DELETE"]
     }
 })
